aa0684d8e95ca5786ebec13c79a865db43e05a52
miminsouls <zuluagamaria04@gmail.com>
2025-03-06 10:21:01 -0500

    Update s_t.py

--- s_t.py
+++ s_t.py
@@ -128,3 +128,51 @@ if result:
     elif english_accent == "Español":
         tld = "com.mx"
     elif english_accent == "Reino Unido":
+        tld = "co.uk"
+    elif english_accent == "Estados Unidos":
+        tld = "com"
+    elif english_accent == "Canada":
+        tld = "ca"
+    elif english_accent == "Australia":
+        tld = "com.au"
+    elif english_accent == "Irlanda":
+        tld = "ie"
+    elif english_accent == "Sudáfrica":
+        tld = "co.za"
+    
+    def text_to_speech(input_language, output_language, text, tld):
+        translation = translator.translate(text, src=input_language, dest=output_language)
+        trans_text = translation.text
+        tts = gTTS(trans_text, lang=output_language, tld=tld, slow=False)
+        try:
+            my_file_name = text[0:20]
+        except:
+            my_file_name = "audio"
+        tts.save(f"temp/{my_file_name}.mp3")
+        return my_file_name, trans_text
+    
+    display_output_text = st.checkbox("Mostrar el texto")
+    
+    if st.button("convertir"):
+        result, output_text = text_to_speech(input_language, output_language, text, tld)
+        audio_file = open(f"temp/{result}.mp3", "rb")
+        audio_bytes = audio_file.read()
+        st.markdown(f"## Tú audio:")
+        st.audio(audio_bytes, format="audio/mp3", start_time=0)
+    
+        if display_output_text:
+            st.markdown(f"## Texto de salida:")
+            st.write(f" {output_text}")
+    
+    def remove_files(n):
+        mp3_files = glob.glob("temp/*mp3")
+        if len(mp3_files) != 0:
+            now = time.time()
+            n_days = n * 86400
+            for f in mp3_files:
+                if os.stat(f).st_mtime < now - n_days:
+                    os.remove(f)
+                    print("Deleted ", f)
+    
+    remove_files(7)
+
